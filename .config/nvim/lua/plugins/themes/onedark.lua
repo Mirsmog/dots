6b66ebc87b0a800545d3441b9d6edf884226ddc2
zwine <tanharka.tyuchiev4@mail.ru>
2024-08-04 05:35:58 +0500

    update nvim config

--- .config/nvim/lua/plugins/themes/onedark.lua
+++ .config/nvim/lua/plugins/themes/onedark.lua
@@ -1,13 +1,17 @@
 return {
   "olimorris/onedarkpro.nvim",
   priority = 1000,
-  opts = {
-    options = {
-      transparency = true, -- Use a transparent background?
-      lualine_transparency = true, -- Center bar transparency?
-      highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
-    },
-  },
+  opts = function()
+    if vim.g.neovide then
+      return {
+        options = {
+          transparency = true, -- Use a transparent background?
+          lualine_transparency = true, -- Center bar transparency?
+          highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
+        },
+      }
+    end
+  end,
   config = function(_, opts)
     require("onedarkpro").setup(opts)
   end,
