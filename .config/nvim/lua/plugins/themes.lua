909db72f77743421828bf9061ecc6cece0dfe192
zwine <tanharka.tyuchiev4@mail.ru>
2024-09-12 04:02:49 +0500

    delete unused themes

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,14 +1,9 @@
 local theme = "nordic"
 
 return {
-  require("plugins.themes.kanagawa"),
-  require("plugins.themes.gruvbox"),
-  require("plugins.themes.everforest"),
-  require("plugins.themes.moonfly"),
-  require("plugins.themes.rose-pine"),
   require("plugins.themes.nordic"),
+  require("plugins.themes.kanagawa"),
   require("plugins.themes.onedarkpro"),
-  require("plugins.themes.tokyodark"),
   {
     "LazyVim/LazyVim",
     opts = {
